6e849512623d0d89f14769348138a49027dff201
Charles Hall <charles@typ3.tech>
2021-10-30 00:08:44 -0700

    try setuptools' method first, then fallback
    Instead of unconditionally doing both. This avoids the need for the user
    to have setuptools installed in order for this plugin to function
    properly.

--- freecad/__init__.py
+++ freecad/__init__.py
@@ -1,2 +1,5 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__import__("pkg_resources").declare_namespace(__name__)
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
