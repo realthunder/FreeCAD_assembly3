dcfe95ec6e4590e73a2e7e5da44c7e883c5bec1d
hasecilu <hasecilu@tuta.io>
2024-11-30 12:26:39 -0600

    fixup! Update translations from CrowdIn

--- freecad/asm3/translations/update_translation.sh
+++ freecad/asm3/translations/update_translation.sh
@@ -39,7 +39,7 @@
 # NOTE: WORKFLOW MAINTAINER (CROWDIN)
 # - Execute the script passing the '-U' flag
 # 	$ ./update_translation.sh -U
-# - Upload the updated file to Crowdin and wait for translators do their thing ;-)
+# - Upload the updated file to CrowdIn and wait for translators do their thing ;-)
 # - Once done, download the translated files, copy them to `freecad/asm3/translations`
 # 	and release all the files to update the changes
 # 	$ ./update_translation.sh -R
@@ -79,16 +79,38 @@ update_locale() {
 	fi
 }
 
+normalize_crowdin_files() {
+	# Rename files which locales are different on FreeCAD and delete not supported locales
+	crowdin_fixes=(af-ZA ar-SA be-BY bg-BG ca-ES cs-CZ da-DK de-DE el-GR eu-ES fi-FI
+		fil-PH fr-FR gl-ES hr-HR hu-HU it-IT ja-JP ka-GE kab-KAB ko-KR lt-LT nl-NL
+		no-NO pl-PL ro-RO ru-RU sk-SK sl-SI sr-SP tr-TR uk-UA vi-VN)
+
+	crowdin_deletes=(az-AZ bn-BD br-FR bs-BA en en-GB en-US eo-UY es-CO es-VE et-EE fa-IR he-IL
+		hi-IN hy-AM id-ID kaa lv-LV mk-MK ms-MY sat-IN si-LK ta-IN te-IN th-TH ur-PK xav yo-NG)
+
+	for pattern in "${crowdin_fixes[@]}"; do
+		find . -type f -name "*_${pattern}\.*" | while read -r file; do
+			new_name=${file//-*./.}
+			mv -v "$file" "$new_name"
+		done
+	done
+
+	for pattern in "${crowdin_deletes[@]}"; do
+		find . -type f -name "*_${pattern}\.*" -delete
+	done
+}
+
 help() {
 	echo -e "\nDescription:"
 	echo -e "\tCreate, update and release translation files."
 	echo -e "\nUsage:"
 	echo -e "\t./update_translation.sh [-R] [-U] [-r <locale>] [-u <locale>]"
 	echo -e "\nFlags:"
-	echo -e "  -R\n\tRelease all locales"
-	echo -e "  -U\n\tUpdate main translation file (locale agnostic)"
+	echo -e "  -R\n\tRelease all translations (qm files)"
+	echo -e "  -U\n\tUpdate all translations (ts files)"
 	echo -e "  -r <locale>\n\tRelease the specified locale"
 	echo -e "  -u <locale>\n\tUpdate strings for the specified locale"
+	echo -e "  -N\n\tNormalize CrowdIn filenames"
 }
 
 # Main function ------------------------------------------------------------------------------------
@@ -99,8 +121,8 @@ LRELEASE=/usr/lib/qt6/bin/lrelease # from Qt6
 # LRELEASE=lrelease                 # from Qt5
 WB="asm3"
 
-# Enforce underscore on locales
-sed -i '3s/-/_/' ${WB}*.ts
+sed -i '3s/-/_/' ${WB}*.ts               # Enforce underscore on locales
+sed -i '3s/\"en\"/\"en_US\"/g' ${WB}*.ts # Use en_US
 
 if [ $# -eq 1 ]; then
 	if [ "$1" == "-R" ]; then
@@ -110,8 +132,13 @@ if [ $# -eq 1 ]; then
 			echo
 		done
 	elif [ "$1" == "-U" ]; then
-		# Update main file (agnostic)
-		update_locale
+		for locale in "${supported_locales[@]}"; do
+			update_locale "$locale"
+		done
+	elif [ "$1" == "-u" ]; then
+		update_locale # Update main file (agnostic)
+	elif [ "$1" == "-N" ]; then
+		normalize_crowdin_files
 	else
 		help
 	fi
@@ -129,7 +156,7 @@ elif [ $# -eq 2 ]; then
 	else
 		echo "Verify your language code. Case sensitive."
 		echo "If it's correct, ask a maintainer to add support for your language on FreeCAD."
-		echo -e "Supported locales, '\033[1;34mFreeCADGui.supportedLocales()\033[m': \033[1;33m"
+		echo -e "\nSupported locales, '\033[1;34mFreeCADGui.supportedLocales()\033[m': \033[1;33m"
 		for locale in $(printf "%s\n" "${supported_locales[@]}" | sort); do
 			echo -n "$locale "
 		done
